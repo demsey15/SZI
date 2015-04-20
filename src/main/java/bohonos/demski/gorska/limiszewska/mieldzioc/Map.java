3580d8db60c04dacd3199c0358d45259f57fae6e
Dominik Demski <dominik.demski@interia.pl>
2015-04-20 18:49:42 +0200

    Mapa

---
+++ src/main/java/bohonos/demski/gorska/limiszewska/mieldzioc/Map.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package bohonos.demski.gorska.limiszewska.mieldzioc;
+
+import java.util.List;
+
+/**
+ * @author Dominik Demski
+ *
+ */
+public class Map {
+
+	public static final int FREE_FIELD = 0;
+	public static final int TABLE = 1;
+	public static final int GREEN_CHAIR = 2;
+	public static final int RED_CHAIR = 3;
+	public static final int CHAIR = 4;
+	
+	public static final int MAP_WIDTH = 21;
+	public static final int MAP_HEIGHT = 17;
+
+	
+	private List<List<Integer>> map;
+	
+}
