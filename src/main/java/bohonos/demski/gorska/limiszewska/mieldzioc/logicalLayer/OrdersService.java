492f92c609a1f57793a8e5d1e19de53c9cac05e4
Dominik Demski <dominik.demski@interia.pl>
2015-06-08 15:24:41 +0200

    OrderService - kolekcje synchronizowane

--- src/main/java/bohonos/demski/gorska/limiszewska/mieldzioc/logicalLayer/OrdersService.java
+++ src/main/java/bohonos/demski/gorska/limiszewska/mieldzioc/logicalLayer/OrdersService.java
@@ -24,14 +24,16 @@ import java.util.Map.Entry;
  */
 public class OrdersService {
     private final List<Meal> menu = new ArrayList<Meal>(); // menu naszej restauracji
-    //private final List<Entry<Integer, Meal>> orders = new ArrayList<Entry<Integer, Meal>>(); //lista zamówieñ, kluczem jest numer stolika - Integer, a wartoœci¹ zamówienie
-    private final List<Entry<Integer, Order>> orders = new ArrayList<Entry<Integer, Order>>(); //lista zamówieñ, kluczem jest numer stolika - Integer, a wartoœci¹ zamówienie
 
-    //private final List<Entry<Integer, Meal>> orders = new ArrayList<Entry<Integer, Meal>>(); //lista zamówieñ, kluczem jest numer stolika - Integer, a wartoœci¹ zamówienie
-    private final List<Entry<Integer, Meal>> tray = new ArrayList<Entry<Integer, Meal>>(); //taca z posi³kami kelnera
-    private final List<Entry<Integer, Meal>> readyMeals = new ArrayList<Entry<Integer, Meal>>(); // zrobione posi³ki przez kuchniê
+
+
     public static final String FILE_PATH = "menu.txt";
     public static OrdersService instance;
+
+    private final List<Order> orders = Collections.synchronizedList(new ArrayList<Order>()); //lista zamówieñ - zrobione przez Dominika
+    private final List<Order> readyMeals = Collections.synchronizedList(new ArrayList<Order>()); // zrobione posi³ki przez kuchniê - zrobione przez Dominika
+    private final List<Order> tray = Collections.synchronizedList(new ArrayList<Order>()); //taca z posi³kami kelnera - zrobione przez Dominika
+
     private OrdersService() throws IOException{
         initList();
         instance = this;
@@ -54,39 +56,6 @@ public class OrdersService {
     }
 
     /**
-     * Metoda zwraca obecn¹ listê zamówieñ.
-     * @return zwraca liste zamówieñ
-     */
-    /*public List<Entry<Integer, Meal>> getOrders(){
-        return orders;
-    }*/
-    public List<Entry<Integer, Order>> getOrders(){
-        return orders;
-    }
-
-
-    /**
-     * Inicjalizuje odczyt dañ z pliku tekstowego do menu.
-     * @throws IOException
-     */
-    private void initList() throws IOException{
-        FileReader fileReader = new FileReader(FILE_PATH);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-        try {
-            String textLine = bufferedReader.readLine();
-            do {
-                //System.out.println(textLine);
-                addMeal(textLine);
-                textLine = bufferedReader.readLine();
-            } while(textLine != null);
-        }
-        finally{
-            bufferedReader.close();
-        }
-    }
-
-    /**
      * Dodaje posi³ek do menu naszej wyœmienitej restauracji.
      * @param line
      */
@@ -100,6 +69,52 @@ public class OrdersService {
         int z = Integer.parseInt(text[5]);
         meal = new Meal(text[0],text[1],w,x,y,z);
         menu.add(meal);
+    }
+
+    /**
+     *  Metoda zwraca tablicê dwuwymiarow¹, która zawiera informacje o pozycji posi³ku w menu i zawartoœci tej pozycji
+     * @return menuList;
+     */
+    public String[][] getMenuList(){
+        String[][] menuList = new String [menu.size()][2];
+        for (int i=0; i<menu.size();i++){
+            menuList[i][0] = Integer.toString(i+1);
+            menuList[i][1] = menu.get(i).getName() +" "+ menu.get(i).getIngredients() ;
+        }
+        return menuList;
+    }
+
+
+    //------------------------------------------------------------------------------------------------------------------
+    /*Dopisane na potrzeby wyswietlania menu i orderow w widoku */
+    /* AL 2015-06-06 */
+    public String[][] getMenuToDisplay(){
+        String[][] menuList = new String [menu.size()][2];
+        for (int i=0; i<menu.size();i++){
+            menuList[i][1] = Integer.toString(i+1);
+            menuList[i][0] = menu.get(i).getName() +" "+ menu.get(i).getIngredients();
+        }
+        return menuList;
+    }
+
+
+    /**
+     * @author Dominik Demski
+     * @return Zwraca kopiê listy zamówieñ.
+     */
+    public List<Order> getOrders(){
+        synchronized (orders){
+            return null;
+        }
+    }
+
+    /**
+     * @author Dominik Demski
+     * Dodaje zamówienie do listy zamówieñ.
+     * @param order
+     */
+    public void addOrder(Order order){
+        orders.add(order);
     }
 
     /**
@@ -140,7 +155,7 @@ public class OrdersService {
             long currentTime = cal.getTimeInMillis();
 
             Order order = new Order(menu.get(a), table, currentTime, vip);
-            orders.add(new AbstractMap.SimpleEntry<Integer, Order>(table, order));
+            orders.add(order);
 
             OrdersPanel orders = MainFrame.getInstance().getNewOrdersPanel();
             orders.setOrdersList(this.getOrdersToDisplay());
@@ -157,6 +172,86 @@ public class OrdersService {
         OrdersPanel orders = MainFrame.getInstance().getNewOrdersPanel();
         orders.setOrdersList(this.getOrdersToDisplay());
     }
+
+
+    public String[][] getOrdersToDisplay(){
+        synchronized (orders) {
+            String[][] orderList = new String[orders.size()][2];
+            System.out.print("Orders list: ");
+            for (int i = 0; i < orders.size(); i++) {
+                orderList[i][1] = Integer.toString(i + 1);
+                orderList[i][0] = orders.get(i).meal.getName() + " " + orders.get(i).meal.getIngredients();
+                System.out.print(orderList[i][0]);
+            }
+            return orderList;
+        }
+    }
+
+    /**
+     * Metoda dodaje zamówienie do listy przygotwanych posi³ków, które czekaj¹ na dostarczenie.
+     * @param food
+     */
+    public void addReadyMeals(Order food){
+        readyMeals.add(food);
+    }
+
+    /**
+     * Metoda usuwa wybrany posi³ek z listy przygotwanych posi³ków, po tym jak kelner umieœci³ go na tacy.
+     * @param food
+     */
+    public void removeReadyMeals(Order food){
+        readyMeals.remove(food);
+    }
+
+
+    /**
+     * Metoda dodaje zamówienie do listy, która zawiera obecn¹ zawartoœæ posi³ków kelnera.
+     * @param food
+     */
+    public void addMealToTray(Order food){
+        tray.add(food);
+    }
+
+    /**
+     * @author Dominik Demski
+     * Metoda usuwa wszystkie zamówienia z listy zamówieñ znajduj¹cych siê na tacy.
+     * Wywo³ywana po dostarczeniu odpowiedniego posi³ku do odpowiedniego stolika.
+     * @param tableNumber numer stolika, dla którego potrawy z tacy maj¹ zostaæ usuniête.
+     */
+    public void removeMealForTableFromTray(int tableNumber){
+        synchronized (tray){
+            for(Order order : tray){
+                if(order.tableNumber == tableNumber){
+                    tray.remove(order);
+                }
+            }
+        }
+    }
+
+    /**
+     * Inicjalizuje odczyt dañ z pliku tekstowego do menu.
+     * @throws IOException
+     */
+    private void initList() throws IOException{
+        FileReader fileReader = new FileReader(FILE_PATH);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+        try {
+            String textLine = bufferedReader.readLine();
+            do {
+                //System.out.println(textLine);
+                addMeal(textLine);
+                textLine = bufferedReader.readLine();
+            } while(textLine != null);
+        }
+        finally{
+            bufferedReader.close();
+        }
+    }
+
+
+
+
 
 
     /**
@@ -177,75 +272,13 @@ public class OrdersService {
         return orderList;
     }*/
 
-    /**
-     *  Metoda zwraca tablicê dwuwymiarow¹, która zawiera informacje o pozycji posi³ku w menu i zawartoœci tej pozycji
-     * @return menuList;
-     */
-    public String[][] getMenuList(){
-        String[][] menuList = new String [menu.size()][2];
-        for (int i=0; i<menu.size();i++){
-            menuList[i][0] = Integer.toString(i+1);
-            menuList[i][1] = menu.get(i).getName() +" "+ menu.get(i).getIngredients() ;
-        }
-        return menuList;
-    }
+
 
 
     //------------------------------------------------------------------------------------------------------------------
-    /*Dopisane na potrzeby wyswietlania menu i orderow w widoku */
-    /* AL 2015-06-06 */
-    public String[][] getMenuToDisplay(){
-        String[][] menuList = new String [menu.size()][2];
-        for (int i=0; i<menu.size();i++){
-            menuList[i][1] = Integer.toString(i+1);
-            menuList[i][0] = menu.get(i).getName() +" "+ menu.get(i).getIngredients();
-        }
-        return menuList;
-    }
 
-    public String[][] getOrdersToDisplay(){
-        String[][] orderList = new String [orders.size()][2];
-        System.out.print("Orders list: ");
-        for (int i=0; i<orders.size();i++){
-            orderList[i][1] = Integer.toString(i+1);
-            orderList[i][0] = orders.get(i).getValue().meal.getName() +" "+ orders.get(i).getValue().meal.getIngredients() ;
-            System.out.print(orderList[i][0]);
-        }
 
-        return orderList;
-    }
-    //------------------------------------------------------------------------------------------------------------------
 
-    /**
-     * Metoda dodaje posi³ek do listy, która zawiera obecn¹ zawartoœæ posi³ków kelnera.
-     * @param food
-     */
-    public void addMealToTray(Entry<Integer, Meal> food){
-        tray.add(food);
-    }
 
-    /**
-     * Metoda usuwa i-ty posi³ek z listy posi³ków znajduj¹cych siê na tacy. Wywo³ywana po dostarczeniu odpowiedniego posi³ku do odpowiedniego stolika.
-     * @param i
-     */
-    public void removeMealFromtray(int i){
-        tray.remove(i);
-    }
-
-    /**
-     * Metoda dodaje posi³ek do listy przygotwanych posi³ków, które czekaj¹ na dostarczenie.
-     * @param food
-     */
-    public void addReadyMeals(Entry<Integer, Meal> food){
-        readyMeals.add(food);
-    }
-
-    /**
-     * Metoda usuwa wybrany posi³ek z listy przygotwanych posi³ków, po tym jak kelner umieœci³ go na tacy.
-     * @param food
-     */
-    public void removeReadyMeals(Entry<Integer, Meal> food){
-        readyMeals.remove(food);
-    }
 
 }
