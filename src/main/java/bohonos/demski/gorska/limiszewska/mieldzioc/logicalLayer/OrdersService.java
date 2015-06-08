cdf7929b34c30a0fab1a5fa8e3281927009d3f39
Dominik Demski <dominik.demski@interia.pl>
2015-06-08 11:26:57 +0200

    zmiany w OrdersService - Agnieszki

--- src/main/java/bohonos/demski/gorska/limiszewska/mieldzioc/logicalLayer/OrdersService.java
+++ src/main/java/bohonos/demski/gorska/limiszewska/mieldzioc/logicalLayer/OrdersService.java
@@ -9,16 +9,14 @@ package bohonos.demski.gorska.limiszewska.mieldzioc.logicalLayer;
  *
  * @author Delirus
  */
+import ViewLayer.MainFrame;
 import ViewLayer.OrdersPanel;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.AbstractMap.SimpleEntry;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.Map.Entry;
-import java.util.Random;
 
 /**
  *Klasa obs³uguj¹ca zamówienia klientów w restauracji
@@ -26,13 +24,25 @@ import java.util.Random;
  */
 public class OrdersService {
     private final List<Meal> menu = new ArrayList<Meal>(); // menu naszej restauracji
-   // private final List<Entry<Integer, Meal>> orders = new ArrayList<Entry<Integer, Meal>>(); //lista zamówieñ, kluczem jest numer stolika - Integer, a wartoœci¹ zamówienie
-    private final
+    //private final List<Entry<Integer, Meal>> orders = new ArrayList<Entry<Integer, Meal>>(); //lista zamówieñ, kluczem jest numer stolika - Integer, a wartoœci¹ zamówienie
+    private final List<Entry<Integer, Order>> orders = new ArrayList<Entry<Integer, Order>>(); //lista zamówieñ, kluczem jest numer stolika - Integer, a wartoœci¹ zamówienie
+
+    //private final List<Entry<Integer, Meal>> orders = new ArrayList<Entry<Integer, Meal>>(); //lista zamówieñ, kluczem jest numer stolika - Integer, a wartoœci¹ zamówienie
     private final List<Entry<Integer, Meal>> tray = new ArrayList<Entry<Integer, Meal>>(); //taca z posi³kami kelnera
     private final List<Entry<Integer, Meal>> readyMeals = new ArrayList<Entry<Integer, Meal>>(); // zrobione posi³ki przez kuchniê
     public static final String FILE_PATH = "menu.txt";
-    public OrdersService() throws IOException{
+    public static OrdersService instance;
+    private OrdersService() throws IOException{
         initList();
+        instance = this;
+    }
+
+    public static OrdersService getInstance() throws IOException {
+        if (instance == null){
+            return new OrdersService();
+        }else{
+            return instance;
+        }
     }
 
     /**
@@ -47,7 +57,10 @@ public class OrdersService {
      * Metoda zwraca obecn¹ listê zamówieñ.
      * @return zwraca liste zamówieñ
      */
-    public List<Entry<Integer, Meal>> getOrders(){
+    /*public List<Entry<Integer, Meal>> getOrders(){
+        return orders;
+    }*/
+    public List<Entry<Integer, Order>> getOrders(){
         return orders;
     }
 
@@ -104,8 +117,34 @@ public class OrdersService {
                 a = r.nextInt(menu.size());
             }while(num.contains(a));
             num.add(a);
-            orders.add(new SimpleEntry<Integer, Meal>(table, menu.get(a)));
+            //orders.add(new SimpleEntry<Integer, Meal>(table, menu.get(a)));
+
             //System.out.println("Doda³em potrawê nr: "+a);
+
+            //----------------------------------------------------------------------------------------------------------
+            /*dopisane na potrzeby wyswietlania listy zamowien
+             * AL 2015-06-06 */
+            Random tableRandom = new Random();
+            Control control = Control.getInstance();
+            int maxTables = control.getAllTablesCoordinates().size();
+            table = tableRandom.nextInt(maxTables-1)+1;
+            boolean vip;
+            /*if (table%2 == 0 && table > 15 ) {
+                vip = true;
+            }else {
+                vip = false;
+            }*/
+            vip = true;
+
+            Calendar cal = Calendar.getInstance();
+            long currentTime = cal.getTimeInMillis();
+
+            Order order = new Order(menu.get(a), table, currentTime, vip);
+            orders.add(new AbstractMap.SimpleEntry<Integer, Order>(table, order));
+
+            OrdersPanel orders = MainFrame.getInstance().getNewOrdersPanel();
+            orders.setOrdersList(this.getOrdersToDisplay());
+            //----------------------------------------------------------------------------------------------------------
         }
     }
 
@@ -115,20 +154,28 @@ public class OrdersService {
      */
     public void removeOrder(int index){
         orders.remove(index);
+        OrdersPanel orders = MainFrame.getInstance().getNewOrdersPanel();
+        orders.setOrdersList(this.getOrdersToDisplay());
     }
+
 
     /**
      * Metoda zwraca tablicê dwuwymiarow¹, która zawiera informacje o sk³adzie zamówienia oraz o numerze stolika, do którego ma byæ dostarczone to zamówienie
      * @return orderList;
      */
-    public String[][] getOrdersList(){
+    /*public String[][] getOrdersList(){
         String[][] orderList = new String [orders.size()][2];
         for (int i=0; i<orders.size();i++){
+
+            orderList[i][0] = Integer.toString(i+1);
+            orderList[i][1] = orders.get(i).getValue().meal.getName() + orders.get(i).getValue().meal.getIngredients() ;
+
             orderList[i][1] = Integer.toString(orders.get(i).getKey());
             orderList[i][0] = orders.get(i).getValue().getName() + orders.get(i).getValue().getIngredients() ;
+
         }
         return orderList;
-    }
+    }*/
 
     /**
      *  Metoda zwraca tablicê dwuwymiarow¹, która zawiera informacje o pozycji posi³ku w menu i zawartoœci tej pozycji
@@ -142,6 +189,32 @@ public class OrdersService {
         }
         return menuList;
     }
+
+
+    //------------------------------------------------------------------------------------------------------------------
+    /*Dopisane na potrzeby wyswietlania menu i orderow w widoku */
+    /* AL 2015-06-06 */
+    public String[][] getMenuToDisplay(){
+        String[][] menuList = new String [menu.size()][2];
+        for (int i=0; i<menu.size();i++){
+            menuList[i][1] = Integer.toString(i+1);
+            menuList[i][0] = menu.get(i).getName() +" "+ menu.get(i).getIngredients();
+        }
+        return menuList;
+    }
+
+    public String[][] getOrdersToDisplay(){
+        String[][] orderList = new String [orders.size()][2];
+        System.out.print("Orders list: ");
+        for (int i=0; i<orders.size();i++){
+            orderList[i][1] = Integer.toString(i+1);
+            orderList[i][0] = orders.get(i).getValue().meal.getName() +" "+ orders.get(i).getValue().meal.getIngredients() ;
+            System.out.print(orderList[i][0]);
+        }
+
+        return orderList;
+    }
+    //------------------------------------------------------------------------------------------------------------------
 
     /**
      * Metoda dodaje posi³ek do listy, która zawiera obecn¹ zawartoœæ posi³ków kelnera.
@@ -157,8 +230,6 @@ public class OrdersService {
      */
     public void removeMealFromtray(int i){
         tray.remove(i);
-
-        OrdersPanel tray = MainFrame.getInsta
     }
 
     /**
@@ -176,4 +247,5 @@ public class OrdersService {
     public void removeReadyMeals(Entry<Integer, Meal> food){
         readyMeals.remove(food);
     }
+
 }
