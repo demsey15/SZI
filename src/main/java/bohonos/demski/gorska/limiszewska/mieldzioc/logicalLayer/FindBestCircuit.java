61361e755270139ab612403079181f16ae2b13ac
andrzej1990 <andrzej.bohonos1990@gmail.com>
2015-06-10 06:03:56 +0200

    naprawione i działające FindBestCircuit

--- src/main/java/bohonos/demski/gorska/limiszewska/mieldzioc/logicalLayer/FindBestCircuit.java
+++ src/main/java/bohonos/demski/gorska/limiszewska/mieldzioc/logicalLayer/FindBestCircuit.java
@@ -5,6 +5,7 @@
  */
 package bohonos.demski.gorska.limiszewska.mieldzioc.logicalLayer;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -15,7 +16,7 @@ import java.util.Set;
  * @author Andrzej
  */
 public class FindBestCircuit {
-    
+
     /**
      * optymalizuje trasê kelnera
      * @param sPaths lista najkrótszych œcie¿ek pomiêdzy poszczególnymi stolikami i miejscem startowym
@@ -23,31 +24,31 @@ public class FindBestCircuit {
      * @return lista stolików (na pierwszym i ostatnim miejscu znajduje siê pozycja pocz¹tkowa)
      */
     public ArrayList<Integer> getOrderList(ShortestPaths sPaths, Set<Integer> vert) {
-        
+
         ShortestPaths shortestPaths = sPaths;
         Set<Integer> vertices = vert;
         int numberOfVertices = vertices.size();
         HashMap<Integer,HashMap<Integer,Integer>> availablePaths = new HashMap<Integer,HashMap<Integer,Integer>>();
         Set<Integer> visited = new HashSet<Integer>();
         Set<Integer> toVisit = new HashSet<Integer>();
-        int next[] = new int[numberOfVertices];
+        int next[] = new int[22];
         next[0] = 0;
         int shortestPathSource = 0;
         int shortestPathDestination = 0;
         int shortestPathLength;
         ArrayList<Integer> outputList = new ArrayList<Integer>();
         int currentVertex = 0;
-        
+
         outputList.add(0);
-        
+
         toVisit.addAll(vertices);
-        
+
         visited.add(0);
         availablePaths.put(0, new HashMap<Integer,Integer>());
         for (int vertex : toVisit) {
             availablePaths.get(0).put(vertex, shortestPaths.getPathLength(0, vertex));
         }
-        
+
         while (toVisit.isEmpty()==false) {
             shortestPathLength = 200;
             for (int source : visited) {
@@ -62,22 +63,34 @@ public class FindBestCircuit {
             next[shortestPathDestination] = next[shortestPathSource];
             next[shortestPathSource] = shortestPathDestination;
             visited.add(shortestPathDestination);
-            toVisit.remove(shortestPathDestination);
+            if (toVisit.contains(shortestPathDestination))
+                toVisit.remove(shortestPathDestination);
             for (int vertex : visited) {
-                availablePaths.get(vertex).remove(shortestPathDestination);
+                if (availablePaths.containsKey(vertex))
+                    if (availablePaths.get(vertex).containsKey(shortestPathDestination))
+                        availablePaths.get(vertex).remove(shortestPathDestination);
             }
             availablePaths.put(shortestPathDestination, new HashMap<Integer,Integer>());
             for (int vertex : toVisit) {
                 availablePaths.get(shortestPathDestination).put(vertex, shortestPaths.getPathLength(shortestPathDestination, vertex));
             }
+            /*int currentV=0;
+            ArrayList<Integer> outputL = new ArrayList<Integer>();
+            outputL.add(0);
+            do {
+                
+                currentV=next[currentV];
+                outputL.add(currentV);
+            } while (currentV != 0);
+            this.printListOfPlaces(outputL);*/
         }
-        
-        
+
+
         do {
             currentVertex=next[currentVertex];
             outputList.add(currentVertex);
         } while (currentVertex != 0);
-        
+
         return outputList;
     }
 
@@ -88,9 +101,37 @@ public class FindBestCircuit {
     public void printListOfPlaces(ArrayList<Integer> list) {
         System.out.println("");
         for (int i : list) {
-            System.out.print(list.get(i) + " ");
+            System.out.print(i + " ");
         }
         System.out.println("");
+    }
+
+
+
+    public static void main(String[] args) {
+        Control control = Control.getInstance();
+        System.out.println("Utworzy³em control.");
+        try {
+            control.prepareMap();
+            System.out.println("Przygotowa³em mapê.");
+            ShortestPaths shortestPaths = new ShortestPaths();
+            //shortestPaths.printGrapf();
+            //shortestPaths.printPathsTo(7);
+            HashSet<Integer> vertices = new HashSet<Integer>();
+            vertices.add(3);
+            vertices.add(11);
+            vertices.add(16);
+            vertices.add(8);
+            vertices.add(19);
+            vertices.add(13);
+            System.out.println("Zbiór utworzony.");
+            FindBestCircuit best = new FindBestCircuit();
+            System.out.println("Obiekt klasy FindBestCircuit stworzony.");
+            best.printListOfPlaces(best.getOrderList(shortestPaths, vertices));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Wyjatek przy tworzeniu mapy.");
+        }
     }
 
 }
